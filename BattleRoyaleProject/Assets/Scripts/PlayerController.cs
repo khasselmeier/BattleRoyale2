0ff73e5c6237710cccbe5966e3c40e1a566cd56f
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-10-05 22:02:22 -0700

    fixed UI health bar displaying weird

--- BattleRoyaleProject/Assets/Scripts/PlayerController.cs
+++ BattleRoyaleProject/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Security.Cryptography;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
